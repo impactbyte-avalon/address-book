a1a7f3fab7fbd743971a4991e46be000e5b4affe
Huda Ridwan <kerupuk.lovers@gmail.com>
2017-10-09 15:30:08 +0700

    Store data to localstorage

--- js/index.js
+++ js/index.js
@@ -11,17 +11,35 @@ function save() {
     console.log(newContactObject);
 
     newContactParagraph = `
-	name: ${newContactObject.name}
-	email: ${newContactObject.email}
-	phone: ${newContactObject.phone}
+	Name: ${newContactObject.name}
+	Email: ${newContactObject.email}
+	Phone: ${newContactObject.phone}
   `;
     console.log(newContactParagraph);
 
     //document.getElementById('contactResult').innerHTML = newContactParagraph;
 
     const listItem = document.createElement("li");
-    var t = document.createTextNode(newContactParagraph);
-    listItem.appendChild(t);
+    var nodeContact = document.createTextNode(newContactParagraph);
+    listItem.appendChild(nodeContact);
     document.getElementById("contactResult").appendChild(listItem);
 
+    // Put the object into storage
+    const arrContact = [];
+    arrContact.push(newContactObject)
+
+    localStorage.setItem('contact',JSON.stringify(arrContact));
+    // Retrieve the object from storage
+    var retrievedObject = JSON.parse(localStorage.getItem('arrContact'));
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+    console.log(retrievedObject);
 };
+
+// const objToStorage = function(newContactObject) {
+//     // body...
+//     // Put the object into storage
+//     localStorage.setItem('book', newContactObject);
+//     // Retrieve the object from storage
+//     var retrievedObject = localStorage.getItem('newContactObject');
+//     console.log('retrievedObject: ', JSON.parse(retrievedObject));
+// }
